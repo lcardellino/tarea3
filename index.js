1350bbc3bdddfb173840f55d28501447cc763f26
Lucas Cardellino <lcardellino@gmail.com>
2022-11-19 09:07:13 -0300

    renamed:    indexController.js -> controllers/indexController.js 	renamed:    productoController.js -> controllers/productoController.js 	renamed:    db.js -> db/db.js 	renamed:    productos.js -> models/productos.js 	modified:   package-lock.json 	modified:   package.json 	renamed:    index.js -> routes/index.js 	renamed:    producto.js -> routes/producto.js

--- index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const {index} = require("../controllers/indexController")
-
-
-router.post('/', index)
-
-
-module.exports = router 
--- indexController.js
+++
@@ -1,5 +0,0 @@
-const index = (req, res) => {
-    res.status(200).send('Tarea numero 3 Back-End')   
-};
-
-module.exports = {index}
